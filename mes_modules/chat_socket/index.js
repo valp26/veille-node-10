5a5ea0d5148be26c846c3e79202aae9598b629d1
valp26 <valp26@outlook.com>
2018-03-08 10:45:59 -0500

    Création de la fonctionnalité pour transmettre un message

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -1,3 +1,4 @@
+const util = require('util')
 let socketio = require('socket.io')
 
 module.exports.listen = function(server){
@@ -7,6 +8,7 @@ module.exports.listen = function(server){
     let objUtilisateur = {}
     io.on('connection', function(socket){
     console.log(socket.id)
+    	// ------------------------------ Traitement de l'utilisateur
     	socket.on('setUser', function(data) {
     		objUtilisateur[socket.id] = data.user
     		//console.log("objUtilisateur =" + util.inspect(objUtilisateur))
@@ -14,7 +16,18 @@ module.exports.listen = function(server){
     		socket.emit('valide_user', data)
     		io.sockets.emit('diffuser_list_user', objUtilisateur)
     	})
-    // .......
+    	// ------------------------------ Traitement des messages
+    	socket.on('setMessage', function(data) {
+    		console.log('message reçu = ' + util.inspect(data))
+    		socket.broadcast.emit('diffuser_message', data)
+    		socket.emit('valide_message', data)
+    	})
+    	// ------------------------------ Traitement de la déconnexion
+    	socket.on('disconnect', function() {
+    		delete objUtilisateur[socket.id]
+    		io.sockets.emit('diffuser_list_user', objUtilisateur)
+    	})
+    // .......fin connexion
    	})
  return io
 }
