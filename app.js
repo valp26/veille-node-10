b36a686fd1fd7714f1ce7e8ccfeea783db26f019
valp26 <valp26@outlook.com>
2018-03-08 09:49:23 -0500

    Création etémission de l'identifiant pour le clavardage

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
 	if (err) return console.log(err)
 	db = database.db('carnet_adresse')
 	// lancement du serveur Express sur le port 8081
-	app.listen(8081, () => {
+	server.listen(8081, () => {
 		console.log('connexion à la BD et on écoute sur le port 8081')
 	})
 })
--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -7,7 +7,14 @@ module.exports.listen = function(server){
     let objUtilisateur = {}
     io.on('connection', function(socket){
     console.log(socket.id)
+    	socket.on('setUser', function(data) {
+    		objUtilisateur[socket.id] = data.user
+    		//console.log("objUtilisateur =" + util.inspect(objUtilisateur))
+    		//console.log(util.inspect(data))
+    		socket.emit('valide_user', data)
+    		io.sockets.emit('diffuser_list_user', objUtilisateur)
+    	})
     // .......
-   })
+   	})
  return io
 }
